d4e622ef0994a3cb4c87e4b817c8cfdd8b351a89
arzy6e4 <abdyva1yevarzy6ek@gmail.com>
2023-02-11 19:37:17 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hwlesson_6/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.hwlesson_6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.bottom_color);
+
+
+
+    }
+}
